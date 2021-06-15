545ad98774450e9906bfc635cb4469cb820ba8db
Lucas Sanchez <lucas.Sanchez@epitech.eu>
2021-06-15 19:01:13 +0200

    feat(context): add headers

--- src/context.ts
+++ src/context.ts
@@ -2,6 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { PubSub } from 'apollo-server';
 
 export interface Context {
+  headers: Record<string, string>
   prisma: PrismaClient
   pubsub: PubSub
 };
@@ -9,7 +10,8 @@ export interface Context {
 const prisma = new PrismaClient();
 const pubsub = new PubSub();
 
-export const context: Context = {
+export const context = ({ req, _ }: any) => ({
+  headers: req.headers,
   prisma: prisma,
   pubsub: pubsub,
-};
+});
