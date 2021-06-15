7994d66a54930d8219992e5e0a0563b748bcbffd
Lucas Sanchez <lucas.Sanchez@epitech.eu>
2021-06-15 19:14:04 +0200

    feat(schema/query): add Connection objectType

--- src/schema/query/types.ts
+++ src/schema/query/types.ts
@@ -1,4 +1,12 @@
+import { objectType } from 'nexus';
 
+const Connection = objectType({
+  name: 'Connection',
+  definition(t) {
+    t.nonNull.string('token');
+  },
+});
 
 export const types = [
+  Connection,
 ];
